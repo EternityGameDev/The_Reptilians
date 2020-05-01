7e3728b6f81918830e948165617c50dffce4c8f0
Zahir9910 <ivan.skaram@gmail.com>
2020-05-01 12:26:18 +0300

    Perekhod Komnat

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void NextLevel(int _ceneNumber)
+    {
+        SceneManager.LoadScene(_ceneNumber);
+    }
+
+
+}
